81c40fdb22e35d3ef2006b7e47931c55eab81096
himanishpuri <himanishpuri2203@gmail.com>
2024-08-27 14:57:39 +0530

    chore: Update database configuration and package dependencies

---
+++ database/dbConfig.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export default async function ConnectDB() {
+	try {
+		const DBResponse = await mongoose.connect(process.env.MONGO_URI, {
+			dbName: process.env.DB_NAME,
+		});
+		console.log(`Database connected to ${DBResponse.connection.name}`);
+	} catch (error) {
+		console.error(`Error connecting to database: ${error}`);
+	}
+}
